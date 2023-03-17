632c8284e391f816d5bc1ecbff31d02d5ff0b59f
sangrimlee <contact@sangrim.dev>
2023-03-17 12:51:21 +0900

    docs: add option documentation
    - add documentation for `Option`

--- packages/tailwindcss-figma-color/src/index.ts
+++ packages/tailwindcss-figma-color/src/index.ts
@@ -5,8 +5,38 @@ import { allVariables, frequentlyUsedVariables } from './constants/variables';
 import { createFigmaColorTheme } from './utils/createColorTheme';
 
 interface Options {
+  /**
+   * The prefix used for the colors. This defaults to `figma`.
+   *
+   * @defaultValue `figma`
+   *
+   * @example
+   * ```html
+   *  <div class="bg-figma-bg-brand"></div>
+   * ```
+   */
   prefix?: string;
+  /**
+   * Whether to add figma theme css variables. This defaults to `false`.
+   *
+   * The following base styles will be added.
+   * ```css
+   * :root {
+   *   --figma-color-bg: 'rgba(255, 255, 255, 1)';
+   *   ...
+   * }
+   * ```
+   *
+   * @defaultValue `false`
+   */
   addRootVariables?: 'light' | 'dark' | false;
+  /**
+   * Whether to use only frequently used variables. This defaults to `false`.
+   *
+   * @see {@link https://www.figma.com/plugin-docs/css-variables/#frequently-used-tokens}
+   *
+   * @defaultValue `false`
+   */
   useOnlyFrequentlyUsed?: boolean;
 }
 
