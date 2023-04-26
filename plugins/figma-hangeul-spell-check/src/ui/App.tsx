1314d57ab8e5a102e6759ba6760f1fa1b37a5180
sangrimlee <contact@sangrim.dev>
2023-04-26 13:28:11 +0900

    feat: add tab instead layout
    - 기존의 Layout이 아닌 Tab 형식의 레이아웃으로 변경

--- plugins/figma-hangeul-spell-check/src/ui/App.tsx
+++ plugins/figma-hangeul-spell-check/src/ui/App.tsx
@@ -1,7 +1,30 @@
-import { Layout } from './components/Layout';
+import { BoxIcon, LayersIcon } from '@radix-ui/react-icons';
+
+import * as Tab from './components/Tab';
+import { SpellCheckByLayer } from './contents/SpellCheckByLayer';
+import { SpellCheckByPage } from './contents/SpellCheckByPage';
 
 const App = () => {
-  return <Layout>한글 맞춤</Layout>;
+  return (
+    <Tab.Root defaultValue="page">
+      <Tab.List aria-label="맞춤법 검사 단위">
+        <Tab.Trigger value="page">
+          <BoxIcon className="mr-2" />
+          Page
+        </Tab.Trigger>
+        <Tab.Trigger value="layer">
+          <LayersIcon className="mr-2" />
+          Layer
+        </Tab.Trigger>
+      </Tab.List>
+      <Tab.Content value="page">
+        <SpellCheckByPage />
+      </Tab.Content>
+      <Tab.Content value="layer">
+        <SpellCheckByLayer />
+      </Tab.Content>
+    </Tab.Root>
+  );
 };
 
 export default App;
