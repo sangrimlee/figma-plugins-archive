76a81ef3b2a29c1308a47398390fe4a1c3cabb1b
sangrimlee <contact@sangrim.dev>
2023-04-27 20:59:49 +0900

    feat: add animate for toast
    - Toast를 위한 animation 관련 tailwindcss 설정 추가

--- plugins/figma-hangeul-spell-check/tailwind.config.js
+++ plugins/figma-hangeul-spell-check/tailwind.config.js
@@ -5,6 +5,34 @@ const isProduction = process.env.NODE_ENV === 'production';
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/ui/index.html', './src/ui/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      keyframes: {
+        hide: {
+          from: {
+            opacity: 1,
+          },
+          to: {
+            opacity: 0,
+          },
+        },
+        slideDown: {
+          from: {
+            opacity: 0,
+            transform: 'translateY(calc(-100% + var(--viewport-padding)))',
+          },
+          to: {
+            opacity: 1,
+            transform: 'translateY(0)',
+          },
+        },
+      },
+      animation: {
+        hideToast: 'hide 150ms ease-in',
+        showToast: 'slideDown 200ms ease-out',
+      },
+    },
+  },
   safelist: [
     /* Color for spell check reason */
     { pattern: /wrong-(red|yelloow|blue|violet)/ },
