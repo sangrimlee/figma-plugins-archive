72d728add6dd821b2ea7e45174f214bae896bc87
sangrimlee <contact@sangrim.dev>
2023-04-27 09:33:05 +0900

    feat: update spell check result item design
    - 맞춤법 검사 결과에 대한 컴포넌트 디자인 수정

--- plugins/figma-hangeul-spell-check/tailwind.config.js
+++ plugins/figma-hangeul-spell-check/tailwind.config.js
@@ -5,6 +5,10 @@ const isProduction = process.env.NODE_ENV === 'production';
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/ui/index.html', './src/ui/**/*.{ts,tsx}'],
+  safelist: [
+    /* Color for spell check reason */
+    { pattern: /wrong-(red|yelloow|blue|violet)/ },
+  ],
   plugins: [
     figmaColorPlugin({
       addRootVariables: isProduction ? false : 'light',
