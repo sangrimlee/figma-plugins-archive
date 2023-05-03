e4b54f7498260140105a5c1212141cc758e732ba
sangrimlee <contact@sangrim.dev>
2023-05-03 20:58:17 +0900

    feat: NodeBadge 컴포넌트 추가

--- plugins/figma-hangeul-ipsum/src/ui/pages/Info.tsx
+++ plugins/figma-hangeul-ipsum/src/ui/pages/Info.tsx
@@ -1,7 +1,11 @@
+import { NodeBadge } from '../components/NodeBadge';
+
 const Info = () => {
   return (
     <div className="flex h-full w-full items-center justify-center">
-      <h1 className="mb-2 text-xl font-semibold">텍스트 레이어를 선택해주세요.</h1>
+      <h1 className="mb-4 flex items-center text-sm font-semibold">
+        텍스트를 생성하고자하는 <NodeBadge type="TEXT" />를 선택해주세요.
+      </h1>
     </div>
   );
 };
