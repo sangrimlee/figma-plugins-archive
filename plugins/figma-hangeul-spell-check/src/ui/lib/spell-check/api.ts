e4faaee105fd519cabd7ef07b0ecd3fa6f6a3ac5
sangrimlee <contact@sangrim.dev>
2023-10-13 17:29:40 +0900

    fix: 맞춤법 검사 API 수정

--- plugins/figma-hangeul-spell-check/src/ui/lib/spell-check/api.ts
+++ plugins/figma-hangeul-spell-check/src/ui/lib/spell-check/api.ts
@@ -1,25 +1,63 @@
 import { getSpellCheckeResult } from './utils';
 
-interface SpellCheckResponse {
+interface SpellCheckResult {
   errata_count: number;
   origin_html: string;
   html: string;
   notag_html: string;
 }
 
-export async function requestSpellCheck(query: string) {
-  const url = new URL('https://m.search.naver.com/p/csearch/ocontent/util/SpellerProxy?color_blindness=0');
+interface SpellCheckResponse {
+  message: {
+    error: string;
+    result: SpellCheckResult;
+  };
+}
+
+export async function requestSpellCheck(query: string, passportKey: string) {
+  const url = new URL('https://m.search.naver.com/p/csearch/ocontent/util/SpellerProxy');
   url.searchParams.append('q', query);
+  url.searchParams.append('color_blindness', '0');
+  url.searchParams.append('passportKey', passportKey);
+  url.searchParams.append('where', 'nexearch');
+
   const res = await fetch(url, { method: 'GET', headers: { referer: 'https://m.search.naver.com' } });
 
   if (!res.ok) {
-    throw Error('네트워크 요청에 실패하였습니다.');
+    throw new Error('NETWORK_REQUEST_FAILED');
   }
 
   const {
     message: {
+      error,
       result: { origin_html: origin, html: result },
     },
-  } = (await res.json()) as { message: { result: SpellCheckResponse } };
+  } = (await res.json()) as SpellCheckResponse;
+
+  if (error === '유효한 키가 아닙니다') {
+    throw new Error('INVALID_KEY');
+  }
+
   return getSpellCheckeResult(origin, result);
+}
+
+export async function requestPassportKey() {
+  const url = new URL('https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8');
+  url.searchParams.set('query', '맞춤법검사');
+  const proxyUrl = `https://corsproxy.io/?${encodeURIComponent(url.toString())}`;
+  const res = await fetch(proxyUrl, {
+    method: 'GET',
+    headers: { referer: 'https://search.naver.com' },
+  });
+
+  if (!res.ok) {
+    throw new Error('NETWORK_REQUEST_FAILED');
+  }
+
+  const html = await res.text();
+  const matched = /passportKey=([a-zA-Z0-9]+)/.exec(html);
+  if (!matched) {
+    throw new Error('CAN_NOT_FIND_PASSPORT_KEY');
+  }
+  return matched[1];
 }
--- plugins/figma-hangeul-spell-check/src/ui/lib/spell-check/index.ts
+++ plugins/figma-hangeul-spell-check/src/ui/lib/spell-check/index.ts
@@ -1,9 +1,9 @@
-import { requestSpellCheck } from './api';
+import { requestPassportKey, requestSpellCheck } from './api';
 import { joinWithLimit, removeDuplicate } from './utils';
 
 export async function spellCheck(characters: string[]) {
-  const queries = joinWithLimit(characters, 500);
-
-  const results = await Promise.all(queries.map((query) => requestSpellCheck(query)));
+  const queries = joinWithLimit(characters, 300);
+  const passportKey = await requestPassportKey();
+  const results = await Promise.all(queries.map((query) => requestSpellCheck(query, passportKey)));
   return removeDuplicate(results.flatMap((result) => result));
 }
